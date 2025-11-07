de70855a22d54108b15163ff045105463596ae64
badger-99 <alpeter91@gmail.com>
2025-11-08 00:19:57 +0300

    Add dark-light toggling

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
 	import '../app.css';
+	import {ModeWatcher} from 'mode-watcher'
+	import {ThemeToggle} from '$lib/components'
 	import favicon from '$lib/assets/favicon.svg';
 
 	let { children } = $props();
@@ -9,4 +11,9 @@
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
+<nav class='mb-4 flex flex-row'>
+	<ThemeToggle />
+</nav>
+
+<ModeWatcher defaultMode='system' />
 {@render children()}
