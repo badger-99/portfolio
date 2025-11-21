9089d803b76a10f8f4465c11f3caf391befb56ca
badger-99 <alpeter91@gmail.com>
2025-11-21 12:39:07 +0300

    Add header logo

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,7 @@
 	import {ModeWatcher} from 'mode-watcher'
 	import {ThemeToggle} from '$lib/components'
 	import favicon from '$lib/assets/favicon.ico';
+	import logo from '$lib/assets/w-LogoHeader1.png'
 
 	let { children } = $props();
 </script>
@@ -13,8 +14,10 @@
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
-<nav class='flex flex-row p-2 fixed w-full h-14 top-0 left-0 justify-between items-center'>
-	<div><a href="#hero">{"<A/>"}</a></div>
+<nav class='flex flex-row pt-2 px-4 fixed w-full h-14 top-0 left-0 justify-between items-center'>
+	<div><a href="#hero">
+		<img alt="header logo" src={logo} width="64"/>
+	</a></div>
 	<div class="flex flex-row gap-2">
 		<a href="#hero" class="hover:underline">Home</a>
 		<a href="#about" class="hover:underline">About</a>
