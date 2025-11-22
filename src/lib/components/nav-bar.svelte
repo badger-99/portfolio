8a63c5c91042dffc8fd44e628eb16590a7ec56be
badger-99 <alpeter91@gmail.com>
2025-11-22 22:19:19 +0300

    Move navbar to components folder

---
+++ src/lib/components/nav-bar.svelte
@@ -0,0 +1,19 @@
+<script>
+  import { ThemeToggle } from '$lib/components';
+	import logo from '$lib/assets/w-LogoHeader1.png';
+</script>
+
+<nav class="fixed top-0 left-0 flex h-14 w-full flex-row items-center px-4 pt-2">
+	<div>
+		<a href="#hero">
+			<img alt="header logo" src={logo} width="64" />
+		</a>
+	</div>
+	<div class="mx-auto flex flex-row gap-2">
+		<a href="#hero" class="hover:underline">Home</a>
+		<a href="#about" class="hover:underline">About</a>
+		<a href="#projects" class="hover:underline">Projects</a>
+		<a href="#contact" class="hover:underline">Contact</a>
+	</div>
+	<!-- <ThemeToggle /> -->
+</nav>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,9 +2,8 @@
 	import './layout.css';
 	import '../app.css';
 	import { ModeWatcher } from 'mode-watcher';
-	import { ThemeToggle } from '$lib/components';
 	import favicon from '$lib/assets/favicon.ico';
-	import logo from '$lib/assets/w-LogoHeader1.png';
+	import NavBar from '$lib/components/nav-bar.svelte';
 
 	let { children } = $props();
 </script>
@@ -15,20 +14,8 @@
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
-<nav class="fixed top-0 left-0 flex h-14 w-full flex-row items-center px-4 pt-2">
-	<div>
-		<a href="#hero">
-			<img alt="header logo" src={logo} width="64" />
-		</a>
-	</div>
-	<div class="mx-auto flex flex-row gap-2">
-		<a href="#hero" class="hover:underline">Home</a>
-		<a href="#about" class="hover:underline">About</a>
-		<a href="#projects" class="hover:underline">Projects</a>
-		<a href="#contact" class="hover:underline">Contact</a>
-	</div>
-	<!-- <ThemeToggle /> -->
-</nav>
+<ModeWatcher defaultMode="dark" />
+<NavBar />
 
-<ModeWatcher defaultMode="system" />
+
 {@render children()}
