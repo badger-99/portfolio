0555bd956ece4d53702fcee0133e2eadb04796a3
badger-99 <alpeter91@gmail.com>
2025-11-22 20:44:43 +0300

    Update TailwindCSS

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,24 +1,27 @@
 <script lang="ts">
+	import './layout.css';
 	import '../app.css';
-	import {ModeWatcher} from 'mode-watcher'
-	import {ThemeToggle} from '$lib/components'
+	import { ModeWatcher } from 'mode-watcher';
+	import { ThemeToggle } from '$lib/components';
 	import favicon from '$lib/assets/favicon.ico';
-	import logo from '$lib/assets/w-LogoHeader1.png'
+	import logo from '$lib/assets/w-LogoHeader1.png';
 
 	let { children } = $props();
 </script>
 
 <svelte:head>
 	<title>Alfred M.</title>
-  <meta name="description" content="Software Engineer & Developer." />
+	<meta name="description" content="Software Engineer & Developer." />
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
-<nav class='flex flex-row pt-2 px-4 fixed w-full h-14 top-0 left-0 items-center'>
-	<div><a href="#hero">
-		<img alt="header logo" src={logo} width="64"/>
-	</a></div>
-	<div class="flex flex-row gap-2 mx-auto ">
+<nav class="fixed top-0 left-0 flex h-14 w-full flex-row items-center px-4 pt-2">
+	<div>
+		<a href="#hero">
+			<img alt="header logo" src={logo} width="64" />
+		</a>
+	</div>
+	<div class="mx-auto flex flex-row gap-2">
 		<a href="#hero" class="hover:underline">Home</a>
 		<a href="#about" class="hover:underline">About</a>
 		<a href="#projects" class="hover:underline">Projects</a>
@@ -27,5 +30,5 @@
 	<!-- <ThemeToggle /> -->
 </nav>
 
-<ModeWatcher defaultMode='system' />
+<ModeWatcher defaultMode="system" />
 {@render children()}
