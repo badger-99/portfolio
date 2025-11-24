3b2278c8cbfc68f4da89bd00b20834e4441606fc
badger-99 <alpeter91@gmail.com>
2025-11-24 13:45:01 +0300

    Update personal data

--- src/lib/data/base.ts
+++ src/lib/data/base.ts
@@ -1,14 +1,19 @@
 const firstName = 'Alfred';
 const lastName = 'Makongoro';
-const title = 'Software Engineer';
+const initials = 'AM'
+const title = 'Software Developer';
+const about =
+	"I build web applications with front-end interfaces and back-end systems that integrate seamlessly. Sometimes it's over a cup of coffee, or with music playing in the background, or both — but always with one goal; bringing ideas to life for the world to see.";
 
 const BaseData = {
 	firstName,
 	lastName,
+	initials,
 	title,
 	get fullName() {
 		return `${firstName} ${lastName}`;
-	}
+	},
+	about,
 };
 
 export default BaseData;
--- src/lib/data/home.ts
+++ src/lib/data/home.ts
@@ -1,8 +1,8 @@
 import BaseData from './base';
 import type { IconType } from 'svelte-awesome/components/Icon.svelte';
-  import at from 'svelte-awesome/icons/at';
-	import github from 'svelte-awesome/icons/github';
-	import linkedinSquare from 'svelte-awesome/icons/linkedinSquare';
+import at from 'svelte-awesome/icons/at';
+import github from 'svelte-awesome/icons/github';
+import linkedinSquare from 'svelte-awesome/icons/linkedinSquare';
 
 const title = 'Home';
 
@@ -11,19 +11,22 @@ const hero: {
 	description: string;
 	links: Array<{ label: string; href: string; icon: IconType }>;
 } = {
-	title: `${BaseData.fullName}`,
+	title: `Hi, I'm ${BaseData.firstName}!`,
 	description:
-		'Self-made Software Engineer passionate about pushing boundaries and bridging ideas and reality',
+		'Full-stack Software Developer, passionate about bridging ideas and reality.',
 	links: [
 		{ label: 'GitHub', href: 'https://github.com/badger-99', icon: github },
-		{ label: 'LinkedIn', href: 'https://linkedin.com/in/alfredmkg/', icon: linkedinSquare },
+		{ label: 'LinkedIn', href: 'https://linkedin.com/in/alfredmkg', icon: linkedinSquare },
 		{ label: 'Email', href: 'mailto:alpeter91@gmail.com', icon: at }
 	]
 };
 
+const initials = BaseData.initials;
+
 const HomeData = {
 	title,
 	hero,
+	initials,
 };
 
 export default HomeData;
