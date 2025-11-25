1b113571612ef63fcfc8a68e07df2af27c41ec92
badger-99 <alpeter91@gmail.com>
2025-11-26 00:14:54 +0300

    Add metadata title component

---
+++ src/lib/components/title.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import BaseData from '$lib/data/base';
+
+	export let title = 'Title';
+</script>
+
+<svelte:head>
+	<title>{`${title} | ${BaseData.meta}`}</title>
+</svelte:head>
--- src/lib/data/base.ts
+++ src/lib/data/base.ts
@@ -1,6 +1,7 @@
 const firstName = 'Alfred';
 const lastName = 'Makongoro';
 const initials = 'AM'
+const meta = 'Alfred M.'
 const title = 'Software Developer';
 const about =
 	"I build web applications with front-end interfaces and back-end systems that fit together naturally, making sure everything feels smooth and works the way it should. Most days it's with a cup of coffee nearby, sometimes with music in the background — but always with the same goal: turning ideas into something real.";
@@ -9,6 +10,7 @@ const BaseData = {
 	firstName,
 	lastName,
 	initials,
+	meta,
 	title,
 	get fullName() {
 		return `${firstName} ${lastName}`;
