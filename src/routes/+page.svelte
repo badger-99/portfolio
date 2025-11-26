b314e7c3184c4a0739f6badf5bc117694d4d7d97
badger-99 <alpeter91@gmail.com>
2025-11-26 18:18:30 +0300

    Remove title from root page component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,6 @@
 <script>
 	import Home from '$lib/components/home.svelte';
-	import Title from '$lib/components/title.svelte';
 </script>
-
-<Title title={'Home'} />
 
 <main class="mt-14 flex h-fit flex-1 snap-y snap-mandatory flex-col px-2">
 	<section id="home">
