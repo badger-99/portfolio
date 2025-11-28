53c57d326eb22b43beacc43e0f0507c6b8848682
badger-99 <alpeter91@gmail.com>
2025-11-28 12:12:27 +0300

    Fix skils separator

--- src/lib/components/skills.svelte
+++ src/lib/components/skills.svelte
@@ -14,9 +14,9 @@
 		{#each groups as group (group.category.slug)}
 			<div class="flex flex-col gap-6">
 				<div class="flex flex-row items-center gap-2">
-					<Separator class="w-[50px]" />
+					<Separator data-slot={'horizontal'} class="data-[orientation=horizontal]:w-[50px]" />
 					<Muted>{group.category.name}</Muted>
-					<Separator class="flex-1" />
+					<Separator data-slot={'horizontal'} class="flex-1" />
 				</div>
 				<div class="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
 					{#each group.items as item (item.slug)}
