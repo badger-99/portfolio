9dcb03b4cdfe84d2d2c53118a17ca6ebdad694ef
badger-99 <alpeter91@gmail.com>
2025-11-29 06:18:41 +0300

    Move toasts to top-center

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -23,7 +23,7 @@
 </svelte:head>
 
 <ModeWatcher defaultMode="dark" />
-<Toaster richColors duration={10000} closeButton />
+<Toaster richColors duration={10000} closeButton position={'top-center'}  />
 <div class="relative flex min-h-screen w-full flex-col overflow-x-hidden">
 	<div class="hidden lg:block"><NavBar /></div>
 	<div class="block lg:hidden fixed top-0 left-0 w-full z-30 pt-2 px-2 bg-linear-to-b from-black from-30% to-transparent">
