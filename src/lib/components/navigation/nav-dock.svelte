50d11e2d3ee5acd772cafa3736d20e5885a90c1c
badger-99 <alpeter91@gmail.com>
2025-11-29 23:24:14 +0300

    Update mobile nav-dock

--- src/lib/components/navigation/nav-dock.svelte
+++ src/lib/components/navigation/nav-dock.svelte
@@ -1,38 +1,22 @@
 <script lang='ts'>
   import NavBarData from '$lib/data/nav-bar';
   import { Icon } from 'svelte-awesome';
-  import * as Tooltip from "$lib/components/ui/tooltip";
   import Dock from "../ui/dock/dock.svelte";
-  import DockIcon from "../ui/dock/dock-icon.svelte";
   import { page } from '$app/state';
+	import Button from '../ui/button/button.svelte';
 </script>
 
 <div class="pointer-events-none fixed inset-x-0 bottom-0 z-30 mx-auto flex h-full max-h-14 origin-bottom bg-linear-to-t from-black from-15% to-transparent">
   <div class="fixed inset-x-0 bottom-3 h-13 w-full ">
     <Dock
       direction="middle"
-      class="pointer-events-auto relative z-50 mx-auto flex h-full min-h-full transform-gpu items-center gap-0.5 rounded-full bg-background/15 backdrop-blur-md px-1 sm:gap-3 md:gap-4"
-      let:mouseX
-      let:distance
-      let:magnification 
+      class="pointer-events-auto relative z-50 mx-auto flex flex-row h-full min-h-full transform-gpu items-center gap-7 sm:gap-8 md:gap-9 justify-between rounded-full bg-background/15 backdrop-blur-md"
     >
       {#each NavBarData.items as item}
-      <a href={item.href} class={`rounded-full ${page.url.pathname === item.href ? 'bg-radial from-white from-10% to-transparent to-55% text-background': ''}`}>
-        <DockIcon {mouseX} {magnification} {distance} class={``}>
-          <Tooltip.Provider>
-          <Tooltip.Root>
-            <Tooltip.Trigger
-              class="hover:bg-zinc-900/80 transition-all duration-200 rounded-full p-3 mx-0"
-            >
-              <!-- <svelte:component this={item.icon} size={22} strokeWidth={1.2} /> -->
-              <Icon data={item.icon} />
-            </Tooltip.Trigger>
-            <Tooltip.Content sideOffset={8}>
-              <p>{item.title}</p>
-            </Tooltip.Content>
-          </Tooltip.Root>
-        </Tooltip.Provider>
-        </DockIcon>
+      <a href={item.href} class={`rounded-full ${page.url.pathname === item.href ? 'bg-white text-background': ''}`}>
+        <Button size='icon' variant='outline' class='rounded-full border-none dark:bg-transparent'>
+        <Icon data={item.icon} />
+      </Button>
       </a>
       {/each}
     </Dock>
